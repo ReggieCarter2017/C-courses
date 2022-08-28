d5f7d3c56cf50932da4dd34e0ee4a088bdf059ac
coolpagani <coolpagani@gmail.com>
2022-08-28 14:24:07 +0300

    second work init commit

--- Program.cs
+++ Program.cs
@@ -2,10 +2,8 @@
 int num2 = 0;
 int max = num1;
 int min = num2;
-if (int.TryParse(Console.ReadLine(), out num1)) > (int.TryParse(Console.ReadLine(), out num2)) {
+if (int.TryParse(Console.ReadLine(), out num1))  (int.TryParse(Console.ReadLine(), out num2))
     Console.WriteLine(max);
-}
-else {
+else
     max = num2;
     Console.Write(max);
-}
