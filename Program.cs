2bdc922112d33674003c8a7421f32f2c4dd6b3ac
coolpagani <coolpagani@gmail.com>
2022-08-28 15:19:13 +0300

    Исправили работу

--- Program.cs
+++ Program.cs
@@ -1,14 +1,8 @@
-﻿int num1 = 0;
-int num2 = 0;
-int num3 = 0;
-int max = 0;
-int num = 0;
-
+﻿int max = 0; int num = 0;
     for (int i = 0; i < 3; i++)
     {
         int.TryParse(Console.ReadLine(), out num);
             if (num >= max){
                 max = num;
             }
-    }
-    Console.Write(max);
+    } Console.Write(max);
