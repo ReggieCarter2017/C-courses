fa56f1a760ef432b4540c83f88ba2ba43c6d6c0e
coolpagani <coolpagani@gmail.com>
2022-08-28 14:33:53 +0300

    Закончили работу со второй задачей

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿int num1 = 0;
 int num2 = 0;
-int max = num1;
-int min = num2;
-if (int.TryParse(Console.ReadLine(), out num1))  (int.TryParse(Console.ReadLine(), out num2))
-    Console.WriteLine(max);
-else
-    max = num2;
-    Console.Write(max);
+int max = int.Parse(Console.ReadLine()!);
+int min = int.Parse(Console.ReadLine()!);
+if (max > min) {
+    Console.Write($"Максимальное число: {max}");
+}
+else {
+    max = min;
+    Console.Write($"Максимальное число: {max}");
+}
