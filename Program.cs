e010aa82f31d53c6a2f20e5e80fd7d71e3fb79b7
coolpagani <coolpagani@gmail.com>
2022-08-28 14:42:11 +0300

    Смотрите ветки

--- Program.cs
+++ Program.cs
@@ -1,11 +1,1 @@
-﻿int num1 = 0;
-int num2 = 0;
-int max = int.Parse(Console.ReadLine()!);
-int min = int.Parse(Console.ReadLine()!);
-if (max > min) {
-    Console.Write($"Максимальное число: {max}");
-}
-else {
-    max = min;
-    Console.Write($"Максимальное число: {max}");
-}
+﻿
